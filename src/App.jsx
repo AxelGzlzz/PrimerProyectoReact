425ff74e96282e886b0d55260e051afa50308018
axel gonzalez <ag180066@gmail.com>
2024-09-13 13:01:11 -0300

    agregamos poco de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   
 
   return (
-   <h1>hola mundo</h1>
+
+    <main className='container my-4'>
+    {/* siempre etiqueta de cierre, todo lo que retorne tiene que estar cerrado en un solo contenedor en react */}
+    <h1 className='text-center'>Primer Proyecto react</h1>
+   <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eius animi officia, alias perferendis repellendus pariatur tempore dolorem inventore <br/> amet veniam praesentium? Necessitatibus nemo in minima labore placeat consectetur ab!</p>
+   </main>
+
   )
 }
 
