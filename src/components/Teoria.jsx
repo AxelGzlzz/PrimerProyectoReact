f28ea7c1d0e4b8fd969e513fdf9c1425805bd6e0
axel gonzalez <ag180066@gmail.com>
2024-09-15 17:59:20 -0300

    agregamos el componente de teoria

---
+++ src/components/Teoria.jsx
@@ -0,0 +1,10 @@
+const Teoria = () => {
+    return (
+        <article>
+           <h3>Componentes</h3> 
+           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus dicta sint non eum maiores unde quia! Sequi expedita debitis quibusdam autem totam quod deleniti perferendis nobis quaerat. Nemo corporis nesciunt, repellendus neque ullam amet ipsum sequi explicabo cumque fugit perspiciatis excepturi ducimus maiores at, exercitationem iusto quaerat eos quod tempora qui porro aliquam modi facere! Tempora quis maxime tenetur, expedita voluptatibus, accusantium nam, alias minima maiores quae vitae animi sit quos dolorum laudantium quidem ipsa aliquam repellat optio! Corporis necessitatibus voluptates doloremque ut incidunt aspernatur corrupti sapiente, nam est quia laborum, pariatur cum. Ad laudantium eius quo assumenda eum! Vitae.</p>
+        </article>
+    );
+};
+
+export default Teoria;
