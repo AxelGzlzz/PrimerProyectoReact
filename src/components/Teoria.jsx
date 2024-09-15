2381e14482b0974e6ffea6ecc2d564d9e0edfd64
axel gonzalez <ag180066@gmail.com>
2024-09-15 18:28:26 -0300

    enviamos props de ejemplo 2

--- src/components/Teoria.jsx
+++ src/components/Teoria.jsx
@@ -4,7 +4,7 @@ const Teoria = (props) => {
            <h3>Componentes</h3> 
            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus dicta sint non eum maiores unde quia! Sequi expedita debitis quibusdam autem totam quod deleniti perferendis nobis quaerat. Nemo corporis nesciunt, repellendus neque ullam amet ipsum sequi explicabo cumque fugit perspiciatis excepturi ducimus maiores at, exercitationem iusto quaerat eos quod tempora qui porro aliquam modi facere! Tempora quis maxime tenetur, expedita voluptatibus, accusantium nam, alias minima maiores quae vitae animi sit quos dolorum laudantium quidem ipsa aliquam repellat optio! Corporis necessitatibus voluptates doloremque ut incidunt aspernatur corrupti sapiente, nam est quia laborum, pariatur cum. Ad laudantium eius quo assumenda eum! Vitae.</p>
        {/* props de padre a hijo */}
-         <h4>Comision: {props.comision} </h4>
+         <h4>Comision: {props.comision} - Año Actual :{props.anioActual} </h4>
         </article>
     );
 };
