d4b4b6f0fa780a0e901490a57723c3f820ff8d76
axel gonzalez <ag180066@gmail.com>
2024-09-17 00:02:22 -0300

    se agrego la funcion de sumar y restar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,15 +1,31 @@
-
+//para crear estados y cuand ocambie el valor se renderize
+import { useState } from "react";
 
 const Contador = () => {
-    return (
-        <section className="my-4">
-<h2 >Contador</h2>
-<h4>10</h4>
-<button className="btn btn-primary me-3">+1</button>
-<button className="btn btn-warning">-1</button>
+  // camelquese
+  // Crear estado STATE funcional components
+  // segundo lugar nombre de una funcion,useState(1) se inicializa en 1 numero
+  const [numero, setNumero] = useState(1);
 
-        </section>
-    );
+
+  const sumar = () => {
+    // con parametross
+    setNumero(numero + 1);
+  };
+
+  return (
+    <section className="my-4">
+      <h2>Contador</h2>
+      <h4>{numero}</h4>
+      {/* si la funcion tiene parametros ()=> sumar(parametro) */}
+      <button className="btn btn-primary me-3" onClick={sumar}>
+        +1
+      </button>
+      {/* tambien se puede hacer en funciones anonimas cuando la funcion es corta */}
+      <button className="btn btn-warning" onClick={()=>setNumero(numero -1)}>-1</button>
+    </section>
+  );
+  //jamas linea aqui
 };
 
 export default Contador;
